8f68e169af9f8f6bb85ff739a40b30cc1065cc66
Ficus Media <>
2025-12-01 15:04:34 +0200

    11-30

--- js/script.js
+++ js/script.js
@@ -12,10 +12,43 @@ var swiper = new Swiper(".servicesSwiper", {
 });
 
 var swiper = new Swiper(".testimonialSwiper", {
-  slidesPerView: 3,
+  slidesPerView: 1.5,
   spaceBetween: 36,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints: {
+    576: {
+      slidesPerView: 2.5,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+  },
 });
+
+
+let swiper;
+
+function initSwiper() {
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth < 992 && !swiper) {
+      swiper = new Swiper(".servicesTabsSwiper", {
+        slidesPerView: "auto",
+        spaceBetween: 16,
+      });
+  }
+
+  if (screenWidth >= 992 && swiper) {
+      swiper.destroy(true, true);
+      swiper = undefined;
+  }
+}
+
+initSwiper();
+window.addEventListener("resize", initSwiper);
